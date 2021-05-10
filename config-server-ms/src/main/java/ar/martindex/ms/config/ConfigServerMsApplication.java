116a46be8543ed2aedcded5026c1634624b3a337
Martin Tapia <mtapia@palmatools.com>
2021-05-10 14:07:52 -0300

    refactor config server

---
+++ config-server-ms/src/main/java/ar/martindex/ms/config/ConfigServerMsApplication.java
@@ -0,0 +1,15 @@
+package ar.martindex.ms.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerMsApplication.class, args);
+    }
+
+}
---
+++ config-server-ms/src/test/java/ar/martindex/ms/config/ConfigServerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.martindex.ms.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
