27cb975d778b441e87dff442842af6af37883a67
Martin Tapia <mtapia@palmatools.com>
2021-05-13 11:46:22 -0300

    Terminado completo: Sección 7: Spring Cloud Security: implementando OAuth2 y JWT

--- oauth-ms/src/main/java/ar/martindex/ms/oauth/servicies/UserAppServiceImpl.java
+++ oauth-ms/src/main/java/ar/martindex/ms/oauth/servicies/UserAppServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ar.martindex.ms.commons.models.entities.UserApp;
 import ar.martindex.ms.oauth.clients.UserAppFeignClient;
+import feign.FeignException;
 
 @Service
 public class UserAppServiceImpl implements UserAppService, UserDetailsService {
@@ -29,21 +30,22 @@ public class UserAppServiceImpl implements UserAppService, UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserApp userApp = userAppFeignClient.findByUsername(username);
 
-        if(userApp == null){
-            logger.error("user not exist: " + username);
-            throw new UsernameNotFoundException("user not exist: " + username);
+        try {
+            UserApp userApp = userAppFeignClient.findByUsername(username);
+            List<GrantedAuthority> authorities = userApp.getRoles()
+                    .stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .peek(authority -> logger.info("role: "+ authority.getAuthority()))
+                    .collect(Collectors.toList());
+
+            logger.info("User authenticated: " + username);
+            return buildUserSpringBootSecurity(userApp, authorities);
+        } catch (FeignException e) {
+            logger.error("user not exist: {}", username);
+            throw new UsernameNotFoundException("user not exist " + username);
         }
 
-        List<GrantedAuthority> authorities = userApp.getRoles()
-                .stream()
-                .map(role -> new SimpleGrantedAuthority(role.getName()))
-                .peek(authority -> logger.info("role: "+ authority.getAuthority()))
-                .collect(Collectors.toList());
-
-        logger.info("User authenticated: " + username);
-        return buildUserSpringBootSecurity(userApp, authorities);
     }
 
     public User buildUserSpringBootSecurity(UserApp userApp, List<GrantedAuthority> authorities){
